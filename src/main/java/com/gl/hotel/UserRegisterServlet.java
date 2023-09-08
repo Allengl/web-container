91c4654f5703aa566cfa3686e3daa1f1220a8973
guolong <guolong.deng@hand-china.com>
2023-09-08 14:11:19 +0800

    httpServletRequest 获取浏览器提交的数据

---
+++ src/main/java/com/gl/hotel/UserRegisterServlet.java
@@ -0,0 +1,27 @@
+package com.gl.hotel;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Map;
+
+public class UserRegisterServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Map<String,String[]> parameterMap = req.getParameterMap();
+        Enumeration<String> parameterNames = req.getParameterNames();
+        String[] parameterUserName = req.getParameterValues("username");
+        String[] parameterAge = req.getParameterValues("age");
+        String[] parameterHobby = req.getParameterValues("hobby");
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+        String hobby = req.getParameter("hobby");
+        System.out.println("dopost is called");
+    }
+
+}
