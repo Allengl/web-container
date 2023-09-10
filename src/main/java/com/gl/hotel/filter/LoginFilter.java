66e09c5b821692c1138677ebfcc5dae5fe42100d
Allen <1942813644@qq.com>
2023-09-10 17:03:29 +0800

    JavaWeb 注解式开发

--- src/main/java/com/gl/hotel/filter/LoginFilter.java
+++ src/main/java/com/gl/hotel/filter/LoginFilter.java
@@ -4,12 +4,14 @@ import com.gl.hotel.cookie.PasswordClass;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebFilter(filterName = "loginFilter", urlPatterns = "/cookie/*")
 public class LoginFilter extends HttpFilter {
 
     @Override
--- src/main/java/com/gl/hotel/listener/MyListener.java
+++ src/main/java/com/gl/hotel/listener/MyListener.java
@@ -3,7 +3,9 @@ package com.gl.hotel.listener;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 
+@WebListener
 public class MyListener implements ServletContextListener {
 
     @Override
